22f11359e36e752d70a728894bd95788da296615
文杰 <1541683150@qq.com>
2021-11-29 10:30:27 +0800

    Create mydb_test.go

---
+++ mydb_test.go
@@ -0,0 +1,46 @@
+package mydb_test
+
+import (
+	"database/sql"
+	"log"
+	"testing"
+
+	_ "github.com/wenj91/taos-driver"
+	mydb "github.com/wenj91/taos-driver"
+)
+
+func TestDb(t *testing.T) {
+	db, err := sql.Open("taoSql", "taoSql://dalong@127.0.0.1/demoapp")
+	if err != nil {
+		t.Errorf("some error %s", err.Error())
+	}
+	rows, err := db.Query("select * from tb1")
+	if err != nil {
+		log.Fatal("some wrong for query", err.Error())
+	}
+	for rows.Next() {
+		var user mydb.MyUser
+		if err := rows.Scan(&user.Name, &user.Age); err != nil {
+			log.Println("scan value erro", err.Error())
+		} else {
+			log.Println(user)
+		}
+	}
+
+	_, err = db.Exec(`insert into tb1 values ("2021-09-04 21:03:38.745", 2)`)
+	log.Println(err)
+}
+
+func Example() {
+	db, err := sql.Open("mydb", "mydb://dalong@127.0.0.1/demoapp")
+	if err != nil {
+		log.Fatalf("some error %s", err.Error())
+	}
+	rows, err := db.Query("select * from demoapp")
+	if err != nil {
+		log.Println("some wrong for query", err.Error())
+	}
+	for rows.Next() {
+		rows.Scan()
+	}
+}
