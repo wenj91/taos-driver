4f3f3c7364bf9841bae493fa28320c2bf0e1f2fc
文杰 <1541683150@qq.com>
2021-11-29 10:31:04 +0800

    Create result.go

---
+++ result.go
@@ -0,0 +1,14 @@
+package mydb
+
+type TaosResult struct {
+	LId int64
+	RAf int64
+}
+
+func (tr *TaosResult) LastInsertId() (int64, error) {
+	return tr.LId, nil
+}
+
+func (tr *TaosResult) RowsAffected() (int64, error) {
+	return tr.RAf, nil
+}
