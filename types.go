01acd41e785d9b3100b3ccaf41df594e0c089e86
文杰 <1541683150@qq.com>
2021-11-29 10:32:01 +0800

    Create types.go

---
+++ types.go
@@ -0,0 +1,25 @@
+package mydb
+
+// 1：BOOL
+// 2：TINYINT
+// 3：SMALLINT
+// 4：INT
+// 5：BIGINT
+// 6：FLOAT
+// 7：DOUBLE
+// 8：BINARY
+// 9：TIMESTAMP
+// 10：NCHAR
+
+const (
+	BOOL_TYPE      = 1
+	TINYINT_TYPE   = 2
+	SMALLINT_TYPE  = 3
+	INT_TYPE       = 4
+	BIGINT_TYPE    = 5
+	FLOAT_TYPE     = 6
+	DOUBLE_TYPE    = 7
+	BINARY_TYPE    = 8
+	TIMESTAMP_TYPE = 9
+	NCHAR_TYPE     = 10
+)
